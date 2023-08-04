c7085a4bfa39652939a8ec1bf43f2787befde7d1
ivan habibi <ivanhabi2@gmail.com>
2023-08-04 10:47:36 +0700

    delete unnecessary code

--- packages/shared/ui/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export const Button = ({ children }) => {
-  return <button className="bg-blue-500 h-[42px] px-6 rounded-lg text-white shadow-lg">{children}</button>;
-};
