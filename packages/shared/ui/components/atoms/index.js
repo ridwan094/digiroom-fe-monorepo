b3edfd05590bf891a0bc42c8f403c9e1bcc12373
fathulbilad <95327806+fathulbilad@users.noreply.github.com>
2023-08-04 09:24:12 +0700

    lupa di commit

--- packages/shared/ui/components/atoms/index.js
+++ packages/shared/ui/components/atoms/index.js
@@ -2,3 +2,5 @@
 export { default as Button } from "./Button";
 export { default as AppLogo } from "./AppLogo";
 export { default as TextInput } from "./TextInput";
+export { default as CheckBox } from "./CheckBox";
+export { default as Tooltip } from "./Tooltip"
--- packages/shared/ui/components/molecules/index.js
+++ packages/shared/ui/components/molecules/index.js
@@ -1,2 +1,4 @@
 // export the components
 export { default as SearchBar } from "./SearchBar";
+export { default as DynamicStepper } from "./StepBar"
+export { default as TabBar} from "./TabBar"
