5346dd77106f5900a0e1954a8bb1c414982f3e68
Muhammad Thariq 'Audah <thariq.audah@gmail.com>
2023-08-04 10:42:56 +0700

    Update layout as on figma

--- packages/shared/ui/components/templates/Layout/index.js
+++ packages/shared/ui/components/templates/Layout/index.js
@@ -3,13 +3,13 @@ import constants from "@/constants";
 import { Footer } from "../../organism";
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <div className={`h-[calc(100vh_-_100px)] bg-slate-100 p-12`}>{children}</div>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className={`bg-white`}>{children}</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Layout;
