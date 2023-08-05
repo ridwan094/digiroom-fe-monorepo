53c124b7e6dcaf1b719fa2c2b89e91c44f51ee1c
DandunAdi <dandun.adi@gmail.com>
2023-08-05 17:29:26 +0700

    add desktop BtnCard component

--- packages/shared/ui/components/atoms/BtnContactUs/index.js
+++ packages/shared/ui/components/atoms/BtnContactUs/index.js
@@ -4,7 +4,7 @@ import { MdOutlineCall } from "react-icons/md";
 const BtnContactUs = (props) => {
   return (
     <button className="flex items-center gap-1 px-1 h-10" {...props}>
-      <MdOutlineCall />
+      <MdOutlineCall className="text-xl" />
       <p className="text-xs">Hubungi Kami</p>
     </button>
   );
