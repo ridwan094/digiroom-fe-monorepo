373798ed0fbd17e2c13b4dc94eeefe37f1b50e71
DandunAdi <dandun.adi@gmail.com>
2023-08-05 20:33:22 +0700

    move news constant data

---
+++ apps/digiroom-news/src/constants/news.js
@@ -0,0 +1,57 @@
+export const MOCK_DATA = {
+  titleHeadPost:
+    'Pahami Hal Mendasar Mengemudi Aman di Jalan Tol, Auto2000 Bantu Jaga  Kondisi Mobil',
+  datePost: '01 Jul 2023',
+  contents: [
+    {
+      title: 'Pahami Kondisi Jalan',
+      subTitle1:
+        'Jika AutoFamily tidak melewati jalan tol yang sama setiap hari, ada risiko tidak mengetahui potensi masalah seperti jalan rusak.',
+      subTitle2:
+        'Bahkan pada saat normal melintas, ada kemungkinan kondisi jalan akan berubah, seperti sedang dilakukan perbaikan jalan yang mengharuskan pengemudi untuk tetap waspada.',
+    },
+    {
+      title: 'Pahami Rambu dan Marka Jalan',
+      subTitle1:
+        'Karena kurang memperhatikan rambu-rambu lalu lintas, ada pengendara yang nekad memotong jalan dari jalur cepat hingga pintu keluar tol.',
+      subTitle2:
+        'Tindakan ini sangat berbahaya karena berisiko tertabrak dari belakang atau menyebabkan mobil terguling.',
+      newsContent: 'https://auto2000.co.id/berita-dan-tips/crm-paa-mobil-raize-apakah-matic-ulasan',
+      subTitle3:
+        'Also make sure not to drive on the shoulder of the toll road because there is a risk of crashing from behind, where this type of accident still occurs frequently.',
+      subTitle4:
+        "Don't forget to obey the speed limit and always keep a safe distance from the car in front.",
+    },
+    {
+      title: 'Pahami Rambu dan Marka Jalan',
+      subTitle1:
+        'Karena kurang memperhatikan rambu-rambu lalu lintas, ada pengendara yang nekad memotong jalan dari jalur cepat hingga pintu keluar tol.',
+      subTitle2:
+        'Tindakan ini sangat berbahaya karena berisiko tertabrak dari belakang atau menyebabkan mobil terguling.',
+      subTitle3:
+        'Also make sure not to drive on the shoulder of the toll road because there is a risk of crashing from behind, where this type of accident still occurs frequently.',
+      subTitle4:
+        "Don't forget to obey the speed limit and always keep a safe distance from the car in front.",
+    },
+    {
+      title: 'Understand the Skill of Each Driver Is Different',
+      subTitle1:
+        "AutoFamily may already have sufficient driving skills.But what about other drivers? They may just be able to drive a car.For that,understand the skills of other drivers by seeing how they. If it's dangerous, avoid it immediately and don't be provoked because it can make them panic and make the wrong decision.",
+    },
+    {
+      title: 'Understand the Importance of Safety Features',
+      subTitle1:
+        'Even though it is the most basic safety feature that protects the most because it is proven to be able to prevent passengers from hitting the glass or being thrown out during an accident.',
+      subTitle2:
+        'Toyota cars are equipped with seat belts up to the 3rd row bench, make the most of this feature and other advanced safety features such as Toyoto Safety Sense',
+    },
+    {
+      title: 'Understand Body Condition',
+      subTitle1:
+        'Because of trivial gaps, many drivers ignore sleep attacks and insist on driving. As a result they hit microsleep and ended in an accident.',
+      subTitle2:
+        'Understand the condition of the body from the potential for drowsiness, fatigue, and dangerous diseases.',
+      newsContent: 'Rush Services can be really be this easy',
+    },
+  ],
+};
