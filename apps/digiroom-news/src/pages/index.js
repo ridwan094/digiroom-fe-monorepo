53dbd4a85de60ef9d053abe0d476597665cb1e3c
DandunAdi <dandun.adi@gmail.com>
2023-08-06 12:05:05 +0700

    add banner news swction

--- apps/digiroom-news/src/pages/index.js
+++ apps/digiroom-news/src/pages/index.js
@@ -1,6 +1,7 @@
 import Layout from 'ui/components/templates/Layout';
 import { BreadCrumbs } from 'ui/components/molecules';
 import { ListArtikel } from 'ui/components/organism';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -12,6 +13,18 @@ export default function Home() {
           </div>
         </div>
         <div className="container mx-auto">
+          <div className="px-4">
+            <h2 className="text-reliableBlack text-2xl font-bold mb-2">Berita & Tips</h2>
+            <p className="text-reliableBlack font-medium mb-6">
+              Dapatkan informasi terkini melalui berita eksklusif dan tips dari Auto2000
+            </p>
+            <div className="relative aspect-video">
+              <Image src="/images/banner.png" alt="" objectFit="cover" objectPosition="0 0" fill />
+              <h3 className="absolute text-white bottom-16 left-14 text-4xl font-bold w-[535px]">
+                Kupas Tuntas Fitur Safety Toyota CHR Hybrid Ini Bikin Makin Pengen Beli
+              </h3>
+            </div>
+          </div>
           {/* List Artikel Desktop */}
           <div className="hidden md:block">
             <ListArtikel />
