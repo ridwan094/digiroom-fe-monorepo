97d7d910d64b07d8885d338c8eab2c78775c7f9f
Danu Prasetyo <danu.prasetyo@digiform.co.id>
2023-08-07 01:21:32 +0700

    change border-b-4 to border-b-2 at OtherPromo

--- packages/shared/ui/components/molecules/OtherPromo/index.js
+++ packages/shared/ui/components/molecules/OtherPromo/index.js
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
-import { FiCalendar } from "react-icons/fi";
-import Text from "../../atoms/Text";
-import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import "./carousel.css";
+import { useState, useEffect } from 'react';
+import { FiCalendar } from 'react-icons/fi';
+import Text from '../../atoms/Text';
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import './carousel.css';
 
 const OtherPromo = ({
   data,
@@ -13,7 +13,7 @@ const OtherPromo = ({
   perPage,
   items,
   title,
-  classNameContainerOtherPromo = "",
+  classNameContainerOtherPromo = '',
 }) => {
   // If response using html string
   // const [article, setArticle] = useState({ __html: "" });
@@ -34,7 +34,7 @@ const OtherPromo = ({
         aria-label="Carousel"
         hasTrack={false}
         options={{
-          gap: "2rem",
+          gap: '2rem',
           type,
           arrows,
           pagination,
@@ -45,14 +45,14 @@ const OtherPromo = ({
           <SplideTrack>
             {items.map((item) => (
               <SplideSlide key={item.src}>
-                <div className="w-full overflow-hidden object-cover flex flex-col justify-center gap-6 border-b-4 border-black">
+                <div className="w-full overflow-hidden object-cover flex flex-col justify-center gap-6 border-b-2 border-black">
                   <img src={item.src} className="w-full" />
                   <div className="px-4 flex flex-col justify-center gap-2 mb-4">
                     <Text.BodySmall className="text-black">
-                      {item.date ? item.date : "-"}
+                      {item.date ? item.date : '-'}
                     </Text.BodySmall>
-                    <Text.Headline5 className="text-black">
-                      {item.headline ? item.headline : "-"}
+                    <Text.Headline5 className="text-black font-bold">
+                      {item.headline ? item.headline : '-'}
                     </Text.Headline5>
                   </div>
                 </div>
@@ -66,10 +66,10 @@ const OtherPromo = ({
 };
 
 OtherPromo.defaultProps = {
-  data: "",
-  classNameContainerOtherPromo: "",
-  title: "",
-  type: "loop",
+  data: '',
+  classNameContainerOtherPromo: '',
+  title: '',
+  type: 'loop',
   arrows: true,
   pagination: true,
   perPage: 1,
