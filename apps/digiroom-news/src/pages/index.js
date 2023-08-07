2548d334e8da82bd9b894c912e5dca7c034a980f
aili <ahmadsuhaili244@gmail.com>
2023-08-07 09:56:21 +0700

    fix ui issue margin on navbar

--- apps/digiroom-news/src/pages/index.js
+++ apps/digiroom-news/src/pages/index.js
@@ -6,16 +6,16 @@ import { newsList } from '../constants/news';
 import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className="relative">
+  return (    
       <Layout>
+        <div className="relative">
         <div className="hidden md:block px-4 border-b border-gray-100 py-1 mb-4">
           <div className="container mx-auto p-4">
             <BreadCrumbs />
           </div>
         </div>
         <div className="container mx-auto">
-          <div className=" px-4 md:px-0 pt-2">
+          <div className=" px-4 md:px-0 pt-2 mb-2">
             <h2 className="text-reliableBlack text-2xl font-bold mb-2">Berita & Tips</h2>
             <p className="text-reliableBlack font-medium mb-6">
               Dapatkan informasi terkini melalui berita eksklusif dan tips dari Auto2000
@@ -36,11 +36,12 @@ export default function Home() {
             </div>
           </div>
           {/* List Artikel Desktop */}
-          <div className="py-4">
+          <div className="px-4 mb-2">
             <ListArtikel itemList={newsList} />
           </div>
         </div>
+        </div>
       </Layout>
-    </div>
+   
   );
 }
