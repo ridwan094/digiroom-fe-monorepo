a233e906e3e83b77fabc0fe037cd4cbee0e818c0
fathulbilad <95327806+fathulbilad@users.noreply.github.com>
2023-08-08 10:14:40 +0700

    go to previous state without column speciall in the first news

--- packages/shared/ui/components/molecules/CardNews/index.js
+++ packages/shared/ui/components/molecules/CardNews/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Card from '../../atoms/Card';
 
-const CardNews = ({ id, title, coverImg, date, index }) => {
-  const isFirstCard = index === 0;
-
-  return (
+const CardNews = ({ id, title, coverImg, date }) => {
+    return (
     <Card className="border border-reliableBlack-310 transition-all ease-in-out duration-300 hover:scale-105">
       <div
         key={id}
-        className={`p-4 lg:p-6 md:p-6 ${isFirstCard ? '' : 'h-full'}`}
+        className="p-4"
       >
         <img
-          className={`w-full h-full object-cover ${isFirstCard ? 'h-40 lg:mb-10 ' : 'h-40 mb-4'}`}
+          className="w-full h-full object-cover"
           src={coverImg}
           alt="Auto2000 promo"
         />
--- packages/shared/ui/components/organism/ListArtikel/index.js
+++ packages/shared/ui/components/organism/ListArtikel/index.js
@@ -23,14 +23,8 @@ const ListArtikel = ({ itemList = [] }) => {
     {
       title: 'NEWS',
       content: (
-        <div className="grid-cols-2 lg:grid grid-cols-3 xl:grid grid-cols-4 gap-4">
-          {ArticleItem.map((item, index) => (
-            <div key={index} className={index === 0 ? 'col-span-2' : 'col-span-1'}>
-              {item}
-            </div>
-          ))}
-        </div>      
-        ),
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">{ArticleItem}</div>
+      ),
     },
     {
       title: 'TIPS & TRICKS',
