8b78d7b4d66c6c9e8e597b4543c1a35690ae8a21
Muhammad Thariq 'Audah <thariq.audah@gmail.com>
2023-08-08 09:20:24 +0700

    update inquiry success component

--- packages/shared/ui/components/organism/InquirySuccess/InquirySuccess.js
+++ packages/shared/ui/components/organism/InquirySuccess/InquirySuccess.js
@@ -1,17 +1,19 @@
-import { MdCheck } from 'react-icons/md';
+import SuccessIcon from 'ui/components/atoms/Icons/SuccessIcon';
+import './InquirySuccess.css';
 
 const InquirySuccess = () => {
   return (
     <div className="container">
-      <MdCheck className="mx-auto mb-8" size={150} />
+      <SuccessIcon />
+
       <div className="leading-5 text-center lg:leading-9">
-        <p className="text-base font-semibold text-reliableBlack mb-1 lg:text-lg">
+        <p className="text-base font-semibold text-reliableBlack mb-2 lg:text-lg">
           Thank you for submitting your inquiry!
         </p>
-        <p className="text-sm font-regular text-reliableBlack">
+        <p className="text-sm font-regular text-reliableBlack mb-4">
           Our team will be in touch with you soon to address your needs.
         </p>
-        <p className="text-sm font-bold uppercase text-[#63CB55] mt-4">Done</p>
+        <p className="text-sm font-bold uppercase text-[#63CB55]">Done</p>
       </div>
     </div>
   );
