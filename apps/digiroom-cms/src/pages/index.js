23e8df5ffcf9f8e3f5baa9e5eaba7eb8c5119b97
Danu Prasetyo <danu.prasetyo@digiform.co.id>
2023-08-21 17:19:49 +0700

    fix LoginPage and add login simulation

--- apps/digiroom-cms/src/pages/index.js
+++ apps/digiroom-cms/src/pages/index.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import DashboardPromo from './dashboard-promo';
+import LoginPage from './login';
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <>
-      <div className="flex flex-row justify-center items-center mt-10">
-        <DashboardPromo />
-      </div>
-    </>
+    <React.Fragment>
+      {isLoggedIn ? <DashboardPromo /> : <LoginPage onLogin={handleLogin} />}
+    </React.Fragment>
   );
 }
