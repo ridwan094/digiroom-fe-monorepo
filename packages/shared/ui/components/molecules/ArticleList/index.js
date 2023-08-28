1ae11eb2a9adf6e42958729513ef2a6384338aa5
Teddy-FN <tedd.ferdy@gmail.com>
2023-08-28 14:34:30 +0700

    Fixing UI new & tips page

--- packages/shared/ui/components/molecules/ArticleList/index.js
+++ packages/shared/ui/components/molecules/ArticleList/index.js
@@ -4,14 +4,11 @@ import { CardNews } from 'ui/components/molecules';
 
 const ArticleList = ({ articles, usingTag }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:gap-x-6 lg:gap-y-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-6">
       {articles.map((article, index) => {
         if (index === 0) {
           return (
-            <Card
-              key={index}
-              className="border-b-[0.5px] border-reliableBlack-310 lg:col-span-2 lg:border-transparent"
-            >
+            <Card key={index} className="border-b-[0.5px] border-reliableBlack-310 bg-reliableBlack3 lg:col-span-3">
               <Link href={`/articles/${article.slug}`}>
                 <img
                   className="w-full h-52 object-cover lg:h-[270px]"
@@ -21,14 +18,14 @@ const ArticleList = ({ articles, usingTag }) => {
               </Link>
 
               {usingTag && (
-                <div className="bg-reliableBlack3 lg:bg-white lg:hidden">
+                <div className="bg-reliableBlack3 lg:bg-reliableBlack3">
                   <Tag className="border-br" serverity="warn">
                     {article.category}
                   </Tag>
                 </div>
               )}
 
-              <div className="pt-2 px-4 pb-4 bg-reliableBlack3 lg:pt-4 lg:px-0 lg:pb-0 lg:bg-white">
+              <div className="pt-2 px-4 pb-4 bg-reliableBlack3 lg:pt-4 lg:px-4 lg:pb-0 lg:bg-reliableBlack3">
                 <Link href={`/articles/${article.slug}`}>
                   <p className="text-sm font-semibold text-reliableBlack mb-1 lg:text-xl lg:font-bold">
                     {article.title}
@@ -37,11 +34,6 @@ const ArticleList = ({ articles, usingTag }) => {
                 <p className="text-xs font-normal text-reliableBlack80 lg:mb-4 lg:text-sm lg:text-reliableBlack">
                   {article.createdDate}
                 </p>
-                <div className="hidden lg:block">
-                  <p className="text-base text-reliableBlack leading-relaxed line-clamp-3">
-                    {article.content}
-                  </p>
-                </div>
               </div>
             </Card>
           );
--- packages/shared/ui/components/molecules/CardNews/index.js
+++ packages/shared/ui/components/molecules/CardNews/index.js
@@ -3,18 +3,14 @@ import { Card, Tag } from '../../atoms';
 
 const CardNews = ({ slug, title, image, date, tag, content, lastArticle }) => {
   return (
-    <Card
-      className={`border-b-[0.5px] border-reliableBlack-310 lg:border-transparent ${
-        lastArticle && 'border-transparent'
-      }`}
-    >
+    <Card className={`border-b-[0.5px] border-reliableBlack-310 mb-8`}>
       {tag && (
-        <Tag className="rounded-br lg:hidden" severity="warn">
+        <Tag className="rounded-br mb-2" severity="warn">
           {tag}
         </Tag>
       )}
-      <div className="flex items-center gap-4 pt-2 px-4 pb-4 lg:flex-col lg:pt-0 lg:px-0 lg:pb-0">
-        <div className="flex-[0_0_128px] h-20 lg:flex-auto lg:flex-auto lg:h-[270px]">
+      <div className="flex items-start gap-4 pt-2 px-4 pb-4 mb-2 lg:flex-row lg:pt-0 lg:px-0 lg:pb-0">
+        <div className="flex-[0_0_150px] h-20 lg:flex-[0_0_150px] lg:flex-[0_0_150px] lg:h-[100px]">
           <Link href={`/articles/${slug}`}>
             <img
               className="w-full h-full object-cover"
@@ -23,18 +19,15 @@ const CardNews = ({ slug, title, image, date, tag, content, lastArticle }) => {
             />
           </Link>
         </div>
-        <div>
+        <div className="flex flex-col gap-4 justify-between px-0 lg:px-0 lg:px-0">
           <Link href={`/articles/${slug}`}>
-            <p className="text-sm font-semibold text-reliableBlack mb-1 line-clamp-2 lg:text-xl lg:font-bold">
+            <p className="text-sm font-semibold text-reliableBlack mb-1 line-clamp-2 lg:text-sm lg:font-bold">
               {title}
             </p>
           </Link>
-          <p className="text-xs font-normal text-reliableBlack50 lg:mb-4 lg:text-sm lg:text-reliableBlack">
+          <p className="text-xs font-normal text-reliableBlack50 lg:text-sm lg:text-reliableBlack50">
             {date}
           </p>
-          <div className="hidden lg:block">
-            <p className="text-base text-reliableBlack leading-relaxed line-clamp-3">{content}</p>
-          </div>
         </div>
       </div>
     </Card>
