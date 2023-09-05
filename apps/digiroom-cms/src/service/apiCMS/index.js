2b96c2b68d5748382dea99b4b676e771fdedc32a
fathulbilad <fathulbilad@gmail.com>
2023-09-05 10:25:06 +0700

    config api cms update, forget to push this one to development

--- apps/digiroom-cms/src/service/apiCMS/index.js
+++ apps/digiroom-cms/src/service/apiCMS/index.js
@@ -16,19 +16,17 @@ const redirectToLogin = () => {
   window.location.href = '/'; // Change the URL as needed
 };
 
+const instance = axios.create({
+  baseURL: getBaseUrl(),
+  headers: {
+    'Cache-Control': 'no-cache, must-revalidate',
+  },
+});
 
-const CMS = () => {
-  let instance = axios.create({
-    baseURL: getBaseUrl(),
-    headers: {
-      'Cache-Control': 'no-cache, must-revalidate',
-    },
-  });
-
-CMS.interceptors.request.use(function (config) {
+instance.interceptors.request.use(function (config) {
   const user = JSON.parse(localStorage.getItem('user'));
 
-  if (user.access_token) {
+  if (user && user.access_token) {
     config.headers.Authorization = `Bearer ${user.access_token}`;
   }
   // Add checking Version APP
@@ -36,20 +34,20 @@ CMS.interceptors.request.use(function (config) {
   return config;
 });
 
-  instance.interceptors.response.use(
-    function (response) {
-      //Add something if needed
-      return response.data;
-    },
-    function (error) {
-      if (error.response && error.response.status === 401) {
-        // Redirect to login page
-        localStorage.removeItem('user');
-        redirectToLogin();
-      }
-      //Add something if needed
-      return Promise.reject(error);
+instance.interceptors.response.use(
+  function (response) {
+    // Add something if needed
+    return response.data;
+  },
+  function (error) {
+    if (error.response && error.response.status === 401) {
+      // Redirect to login page
+      localStorage.removeItem('user');
+      redirectToLogin();
     }
-  );
+    // Add something if needed
+    return Promise.reject(error);
+  }
+);
 
-export default CMS;
+export default instance;
