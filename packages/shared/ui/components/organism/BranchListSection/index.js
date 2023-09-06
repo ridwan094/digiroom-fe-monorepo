7c7b0c63447c2774c60aedefbc511e5c35374351
DandunAdi <dandun.adi@gmail.com>
2023-09-06 16:52:13 +0700

    add map section to branch list page

--- packages/shared/ui/components/organism/BranchListSection/index.js
+++ packages/shared/ui/components/organism/BranchListSection/index.js
@@ -1,32 +1,50 @@
+import { useState } from 'react';
 import { BranchCard, BranchFilter } from '../../molecules';
+import BranchMapSection from '../BranchMapSection';
 
 const BranchListSection = () => {
+  const [isMapView, setIsMapView] = useState(false);
+
+  const handleChangeMapView = () => {
+    setIsMapView((prevValue) => !prevValue);
+  };
+
   return (
-    <section className="md:container">
-      <div className="px-4 md:px-0 py-6">
-        <h1 className="text-xl md:text-2xl font-bold mb-3">
-          Dealer dan Bengkel Toyota di Jakarta Pusat
-        </h1>
-        <p className="text-xs md:text-base">
-          Selamat datang di Auto2000 Jakarta Pusat. Dapatkan informasi terkait promosi terbaru,
-          alamat, jam operasional Auto2000 Jakarta Pusat terbaru disini.
-        </p>
-      </div>
-      <div className="mb-9">
-        <BranchFilter onClickHandlerViewMap={() => {}} />
-      </div>
-      <div className="md:grid grid-cols-3 gap-6">
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
-        <BranchCard />
+    <section className="border-b-4 pb-8">
+      <div className="md:container ">
+        <div className="px-4 md:px-0 py-6">
+          <h1 className="text-xl md:text-2xl font-bold mb-3">
+            Dealer dan Bengkel Toyota di Jakarta Pusat
+          </h1>
+          <p className="text-xs md:text-base">
+            Selamat datang di Auto2000 Jakarta Pusat. Dapatkan informasi terkait promosi terbaru,
+            alamat, jam operasional Auto2000 Jakarta Pusat terbaru disini.
+          </p>
+        </div>
+        <div className="mb-9">
+          <BranchFilter isMapView={isMapView} onClickHandlerViewMap={handleChangeMapView} />
+        </div>
+        {isMapView && (
+          <BranchMapSection
+            onClickClose={handleChangeMapView}
+            onClickDirection={handleChangeMapView}
+          />
+        )}
+        {!isMapView && (
+          <div className="md:grid grid-cols-3 gap-6">
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+            <BranchCard />
+          </div>
+        )}
       </div>
     </section>
   );
